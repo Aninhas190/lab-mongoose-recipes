181e125a402665889264228522dcc71793030309
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-07 18:28:27 +0100

    Completed Lab

--- index.js
+++ index.js
@@ -21,6 +21,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Margharita pizza',
+      level: 'Easy Peasy',
+      ingredients: ['Pizza bread', 'tomato sauce', 'cheese'],
+      cuisine: 'italian',
+      dishType:'main_course',
+      duration: '15',
+      creator: 'Ana'
+    })
+  })
+  .then((recipe) => {
+    console.log('added recipe :', recipe);
+
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    console.log('new recipes added: ', recipes)
+
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  })
+  .then((Updatedrecipe) => {
+    console.log('Updated Recipe', Updatedrecipe);
+
+    return Recipe.deleteOne({title: "Carrot Cake"});
+  })
+  .then((deletedRecipe) => {
+    console.log('The recipe was deleted successfully');
+
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Disconnected from MongoDB');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
